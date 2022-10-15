f59643a287ae4e179b80276f10fa09d89639e66e
Oxygen <grandnorik@mail.ru>
2022-10-15 23:03:00 +0600

    Task2 done

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите два числа: ");
+string userNumber1 = Console.ReadLine();
+string userNumber2 = Console.ReadLine();
+int number1 = int.Parse(userNumber1);
+int number2 = int.Parse(userNumber2);
+if(number1 < number2)
+{
+    Console.WriteLine($"Число {number1} меньше числа {number2}");
+}
+else
+{
+    Console.WriteLine($"Число {number1} больше числа {number2}");
+}
