7bf46ee4e6867699aa1da9a48e8752f02a0e6c8a
Oxygen <grandnorik@mail.ru>
2022-10-15 23:29:16 +0600

    Task4 done

---
+++ Task4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите 3 числа");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+Console.WriteLine($"Число {max} максимальное их введенных чисел");
