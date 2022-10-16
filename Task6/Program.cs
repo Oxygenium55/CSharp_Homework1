d055d77eb05663db79f27f83e01f64ddc5cd0414
Oxygen <grandnorik@mail.ru>
2022-10-16 09:15:18 +0600

    Task6 done

---
+++ Task6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int i = int.Parse(Console.ReadLine());
+if(i % 2 == 0)
+{
+    Console.WriteLine($"Число {i} является четным");
+}
+else
+{
+    Console.WriteLine($"Число {i} является нечетным");
+}
